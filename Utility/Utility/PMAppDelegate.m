6b2978805ad5c6c8692561af33f6ea519112bb47
B.P.SINGH <kunvar.buddhaprakash@parallelminds.co.uk>
2014-06-27 12:47:34 +0530

    Test Log added

--- Utility/Utility/PMAppDelegate.m
+++ Utility/Utility/PMAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"dbg_PMFileManager Branch");
     return YES;
 }
 							
